83bcdb3594ee6c38c04c51994cf16a2f0d172075
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-11-17 18:33:18 -0300

    matriz identidade com dimensões 5 x 5

--- 03-vetores-matrizes-structs-ponteiros/tempCodeRunnerFile.c
+++ 03-vetores-matrizes-structs-ponteiros/tempCodeRunnerFile.c
@@ -1,24 +1,3 @@
-// #include <stdio.h>
-
-// int main()
-// {
-
-//     int mat[4][3];
-//     int i, j;
-
-//     for (i = 0; i < 4; i++)
-//     {
-//         for (j = 0; j < 3; j++)
-//         {
-//             printf("mat[%d][%d]: ", i, j);
-//             scanf("%d", &mat[i][j]);
-//         }
-//     }
-//     return 0;
-// }
-
-/////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 #include <stdlib.h>
 int main()
